1cb00bd8aabe0a7a7ed3290f7755a81db2af8aea
Nikhil_Sai_Nambula <nambula.nikhilsai@gmail.com>
2024-12-23 10:10:11 +0530

    modified imageConstansts file

--- src/main/java/com/products/shop/ImageConstants.java
+++ src/main/java/com/products/shop/ImageConstants.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class ImageConstants {
 	public static final String IMAGE1 = String.join(",",
-			"https://github.com/Nikhil-sai-nambula/e-commerce/blob/main/img1/img1_1.avif",
-			"https://github.com/Nikhil-sai-nambula/e-commerce/blob/main/img1/img1_2.avif",
-			"https://github.com/Nikhil-sai-nambula/e-commerce/blob/main/img1/img1_3.avif",
-			"https://github.com/Nikhil-sai-nambula/e-commerce/blob/main/img1/img1_4.avif",
-			"https://github.com/Nikhil-sai-nambula/e-commerce/blob/main/img1/img1_5.avif");
+			"https://raw.githubusercontent.com/Nikhil-sai-nambula/e-commerce/refs/heads/main/img1/img1_1.avif?token=GHSAT0AAAAAAC36ARDD3YXWLVIPV2UPVTCOZ3I5FIQ",
+			"https://raw.githubusercontent.com/Nikhil-sai-nambula/e-commerce/refs/heads/main/img1/img1_2.avif?token=GHSAT0AAAAAAC36ARDCF5RIW4UE74BUPTFGZ3I5F6Q",
+			"https://raw.githubusercontent.com/Nikhil-sai-nambula/e-commerce/refs/heads/main/img1/img1_3.avif?token=GHSAT0AAAAAAC36ARDDPAL4BBCMPXV4WYTOZ3I5GIQ",
+			"https://raw.githubusercontent.com/Nikhil-sai-nambula/e-commerce/refs/heads/main/img1/img1_4.avif?token=GHSAT0AAAAAAC36ARDCWFXDCNGM4QPQ5Z6CZ3I5GRQ",
+			"https://raw.githubusercontent.com/Nikhil-sai-nambula/e-commerce/refs/heads/main/img1/img1_5.avif?token=GHSAT0AAAAAAC36ARDCUJEDQAZVWIDKJ75WZ3I5G2Q");
 
 }
